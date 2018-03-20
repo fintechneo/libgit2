a60007bc12cfe3cbcc07386294e2bdb1d34b73c4
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2018-03-20 21:12:41 +0100

    Emscripten hacks: Webworker code created from main page

---
+++ emscripten_hacks/stupid_worker.js
@@ -0,0 +1,11 @@
+// "Stupid" web worker that accepts scripts
+
+self.onmessage = (msg) => {  
+    // Accept script from main thread      
+    const result = eval(msg.data);
+    if(result && result.then) {
+        result.then((ret) => postMessage(ret));
+    } else {
+        postMessage(result);
+    }
+};
